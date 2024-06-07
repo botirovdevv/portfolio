504fead0435c5f0f2db4568e111d0c29bf690a3d
botirovdev <botirovdev7@gmail.com>
2024-06-07 12:47:01 +0500

    fixed bugs

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -8,23 +8,22 @@ const Header = () => {
             <div className="container">
                 <div className="header-content">
                     <div>
-                        <Image width={213} src={avatar} alt='Avatar' />
+                        <Image width={213} src={avatar} alt="Avatar" />
                     </div>
                     <h1 className="header-title" data-aos="fade-up">
                         I do code and
-                        make content <span className='header-title_linner'>about it!</span>
+                        make content <span className="header-title_linner">about it!</span>
                     </h1>
                     <p className="header-subtitle">
-                        I am a Frontend developer with 2 years of experience, so far I have built small websites to professional websites. If you want to see what I've done, go to the projects page
+                        I am a Frontend developer with 2 years of experience, so far I have built small websites to professional websites. If you want to see what I&apos;ve done, go to the projects page
                     </p>
-                    <div className='header-buttons'>
+                    <div className="header-buttons">
                         <button className="header-button">
                             Get In Touch
                         </button>
                         <button className="header-btn">
                             Download CV
                         </button>
-
                     </div>
                 </div>
             </div>
